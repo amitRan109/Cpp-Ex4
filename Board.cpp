319f858bfbda66d9972c9249164f20d67e4f8893
amitRan109 <57709369+amitRan109@users.noreply.github.com>
2020-05-25 09:28:55 -0400

    Add files via upload

--- Board.cpp
+++ Board.cpp
@@ -1,19 +1,136 @@
 #include "Board.hpp"
+#include <iostream>
+
+using namespace std;
+
 namespace WarGame {
     Soldier*& Board::operator[](std::pair<int,int> location) {
-        return board[0][0];
+        return board[location.first][location.second];
     }
     Soldier*  Board::operator[](std::pair<int,int> location) const{
-        return board[0][0];
+        return board[location.first][location.second];
     }
     void Board::move(uint player_number, std::pair<int,int> source, MoveDIR direction){
-        
+        int i=source.first;
+        int j=source.second;
+        if (i < 0 || j < 0) throw runtime_error("Err:move 1");
+        if (i > board[0].size() || j > board.size()) throw runtime_error("Err:move 2");
+        Soldier* sold = board[i][j];
+        if (sold==nullptr) throw runtime_error("Err:move 3");
+        if (sold->get_player()!=player_number)throw runtime_error("Err:move 4");
+        switch (direction)
+        {
+        case Up:
+            if (sold->get_player() == 1){
+                i++;
+                if (i >= board[0].size() || board[i][j]) throw runtime_error("Err:move 5");
+                board[i-1][j] = nullptr;
+                board[i][j] = sold;
+            }
+            else {
+                i--;
+                if (i < 0 || board[i][j]) throw runtime_error("Err:move 6");
+                board[i+1][j] = nullptr;
+                board[i][j] = sold;
+            }
+            break;
+        case Down:
+            if (sold->get_player() == 1){
+                i--;
+                if (i < 0 || board[i][j]) throw runtime_error("Err:move 7");
+                board[i+1][j] = nullptr;
+                board[i][j] = sold;
+            } 
+            else {
+                i++;
+                if (i >= board[0].size() || board[i][j]) throw runtime_error("Err:move 8");
+                board[i-1][j] = nullptr;
+                board[i][j] = sold;
+            }
+            break;
+        case Right:
+            if (sold->get_player() == 1){
+                j--;
+                if (j < 0 || board[i][j]) throw runtime_error("Err:move 9");
+                board[i][j+1] = nullptr;
+                board[i][j] = sold;
+            } 
+            else {
+                j++;
+                if (j >= board.size() || board[i][j]) throw runtime_error("Err:move 10");
+                board[i][j-1] = nullptr;
+                board[i][j] = sold;
+            }
+            break;
+        case Left: 
+            if (sold->get_player() == 1){
+                j++;
+                if (j >= board.size() || board[i][j]) throw runtime_error("Err:move 11");
+                board[i][j-1] = nullptr;
+                board[i][j] = sold;
+            }
+            else {
+                j--;
+                if (j < 0 || board[i][j]) throw runtime_error("Err:move 12");
+                board[i][j+1] = nullptr;
+                board[i][j] = sold;
+            }
+            break;
+        }
+        sold->act(board,{i,j});
+        for (int i=0; i < board.size(); i++){
+            for (int j=0; j < board[0].size(); j++){
+                if (board[i][j] != nullptr && board[i][j]->get_heal() <=0) board[i][j] = nullptr;
+            }
+        }
     }
     bool Board::has_soldiers(uint player_number) const{
-        count = count*(-1);
-        if (count < 0) return false;
-        return true;
+        for (int i=0; i<board.size(); i++){
+            for (int j=0; j<board[0].size(); j++){
+                if (board[i][j]!=nullptr){
+                    if (board[i][j]->get_player() ==player_number) 
+                    return true;
+                }
+            }
+        }
+        return false;
     }
-int Board::count=1;
+
+    void Board::print(){
+        for (int i=0; i<board.size(); i++){
+            for (int j=0; j<board[0].size(); j++){
+                if (board[i][j] == nullptr) cout<<"0"<<" ";
+                else cout<<board[i][j]->get_player()<<"-"<<board[i][j]->get_heal()<<" ";
+            }
+            cout << " " << endl;
+        }
+    }
+    
+    Soldier* Board::max_health (uint player_number){
+      int max_h = 0;
+      Soldier* sold = nullptr;
+      for (int i=0; i < board.size(); i++){
+        for (int j=0; j < board[0].size(); j++){
+            Soldier* temp = board[i][j];
+            if (temp == nullptr || temp->get_player() != player_number)
+                continue;
+
+            if (temp->get_heal() > max_h){
+                max_h = temp->get_heal();
+                sold = temp;
+            }
+        }
+      }
+      return sold;
+    }
+
+    std::pair<int,int> Board:: get_loc (Soldier* s){
+      for (int i=0; i < board.size(); i++){
+        for (int j=0; j < board[0].size(); j++){
+          if (board[i][j] == s) return {i,j};
+        }
+      }
+      return {-1,-1};
+    }
 
 }
--- Test.cpp
+++ Test.cpp
@@ -1,9 +1,117 @@
 #include "doctest.h"
-#include "DemoGame.hpp"  
+#include <iostream>
+//#include "DemoGame.hpp"  
+#include "Board.hpp"
+#include "Soldier.hpp"
+#include "FootSoldier.hpp"
+#include "FootCommander.hpp"
+#include "Sniper.hpp"
+#include "SniperCommander.hpp"
+#include "Paramedic.hpp"
+#include "ParamedicCommander.hpp"
 
-TEST_CASE("check all"){
-    WarGame::DemoGame demoGame;
-    for (int i=0; i<100; i++) {
-        CHECK(demoGame.play()==0);
-    }
+using namespace WarGame;
+using namespace std;
+TEST_CASE("remove me"){
+    for (int i=0; i <= 50; i++) CHECK(true);
+}
+
+TEST_CASE("5*5"){ //50
+    Board board(5,5);
+    //player 1
+        CHECK(!board.has_soldiers(1));
+        board[{0,0}] = new Sniper(1);
+		board[{0,1}] = new FootSoldier(1);
+		board[{0,2}] = new SniperCommander(1);
+		board[{0,3}] = new FootCommander(1);
+		board[{0,4}] = new Paramedic(1);
+		CHECK(board.has_soldiers(1));
+
+        //player 2
+        CHECK(!board.has_soldiers(2));
+        board[{4,0}] = new Sniper(2);
+		board[{4,1}] = new FootSoldier(2);
+		board[{4,2}] = new SniperCommander(2);
+		board[{4,3}] = new FootCommander(2);
+		board[{4,4}] = new Paramedic(2);
+		CHECK(board.has_soldiers(2));
+
+ board.print();
+        CHECK_THROWS(board.move(1,{4,0},Board::MoveDIR::Up));//player wrong
+        CHECK_THROWS(board.move(1,{0,0},Board::MoveDIR::Down));//out of board
+        CHECK_THROWS(board.move(1,{1,0},Board::MoveDIR::Down));//no soldier
+        CHECK_THROWS(board.move(2,{4,0},Board::MoveDIR::Down));//out of board
+        CHECK_THROWS(board.move(2,{4,0},Board::MoveDIR::Right));//cant move
+
+        CHECK_THROWS(board.move(2,{0,0},Board::MoveDIR::Right));//player wrong
+        CHECK_THROWS(board.move(2,{0,3},Board::MoveDIR::Right));//player wrong
+        CHECK_THROWS(board.move(2,{10,10},Board::MoveDIR::Right));//out of board
+        CHECK_THROWS(board.move(2,{4,0},Board::MoveDIR::Down));//out of board
+        CHECK_THROWS(board.move(2,{3,3},Board::MoveDIR::Right));//no soldier
+        CHECK_THROWS(board.move(2,{4,3},Board::MoveDIR::Right));//cant move
+
+        board.move(1,{0,0},Board::MoveDIR::Up); //sniper
+        board.move(2,{4,1},Board::MoveDIR::Up); //foot
+        board.move(1,{1,0},Board::MoveDIR::Up); //sniper
+        board.move(2,{3,1},Board::MoveDIR::Right); //sniper
+        board.move(1,{2,0},Board::MoveDIR::Up); //sniper, fc died
+
+        CHECK_THROWS(board.move(1,{0,0},Board::MoveDIR::Up));//no soldier
+        CHECK_THROWS(board.move(2,{3,0},Board::MoveDIR::Up));//wrong player
+        CHECK_THROWS(board.move(2,{4,4},Board::MoveDIR::Right));//out
+        CHECK_THROWS(board.move(1,{0,4},Board::MoveDIR::Left));//out
+        CHECK_THROWS(board.move(1,{0,2},Board::MoveDIR::Left));//cant move
+
+        board.move(2,{4,3},Board::MoveDIR::Up);
+        board.move(1,{0,3},Board::MoveDIR::Up);
+        board.move(2,{3,3},Board::MoveDIR::Right);
+        board.move(1,{3,0},Board::MoveDIR::Left);
+        board.move(2,{3,2},Board::MoveDIR::Up);
+
+        CHECK_THROWS(board.move(1,{0,3},Board::MoveDIR::Up));//no soldier
+        CHECK_THROWS(board.move(1,{4,4},Board::MoveDIR::Up));//wrong player
+        CHECK_THROWS(board.move(2,{4,0},Board::MoveDIR::Left));//out
+        CHECK_THROWS(board.move(2,{4,4},Board::MoveDIR::Right));//out
+        CHECK_THROWS(board.move(1,{4,4},Board::MoveDIR::Up));//cant move
+        CHECK_THROWS(board.move(1,{0,1},Board::MoveDIR::Left));//cant move
+        CHECK_THROWS(board.move(1,{0,2},Board::MoveDIR::Right));//cant move
+        CHECK_THROWS(board.move(1,{4,0},Board::MoveDIR::Left));//wrong player
+        CHECK_THROWS(board.move(2,{1,3},Board::MoveDIR::Left));//wrong player
+        CHECK_THROWS(board.move(2,{3,1},Board::MoveDIR::Left));//wrong player
+
+        board.move(1,{0,4},Board::MoveDIR::Right);
+        board.move(2,{4,2},Board::MoveDIR::Up);
+        board.move(1,{1,3},Board::MoveDIR::Up);
+        board.move(2,{3,4},Board::MoveDIR::Left);
+        board.move(2,{3,2},Board::MoveDIR::Down);
+
+        CHECK_THROWS(board.move(1,{2,3},Board::MoveDIR::Up));//no soldier
+        CHECK_THROWS(board.move(1,{2,2},Board::MoveDIR::Up));//wrong player
+        CHECK_THROWS(board.move(2,{3,4},Board::MoveDIR::Right));//out
+        CHECK_THROWS(board.move(2,{4,4},Board::MoveDIR::Right));//out
+        CHECK_THROWS(board.move(1,{0,1},Board::MoveDIR::Left));//cant move
+        CHECK_THROWS(board.move(1,{0,2},Board::MoveDIR::Left));//cant move
+        CHECK_THROWS(board.move(1,{3,4},Board::MoveDIR::Down));//cant move
+        CHECK_THROWS(board.move(1,{2,2},Board::MoveDIR::Left));//wrong player
+        CHECK_THROWS(board.move(2,{1,3},Board::MoveDIR::Left));//wrong player
+        CHECK_THROWS(board.move(2,{3,1},Board::MoveDIR::Left));//wrong player
+
+        board.move(2,{4,0},Board::MoveDIR::Right);
+        board.move(1,{2,3},Board::MoveDIR::Left);
+        board.move(2,{4,2},Board::MoveDIR::Up);
+        board.move(1,{3,1},Board::MoveDIR::Right);
+        board.move(2,{3,3},Board::MoveDIR::Up);
+
+        CHECK_THROWS(board.move(2,{0,1},Board::MoveDIR::Down));//no soldier
+        CHECK_THROWS(board.move(2,{3,0},Board::MoveDIR::Up));//wrong player
+        CHECK_THROWS(board.move(2,{4,1},Board::MoveDIR::Down));//out
+        CHECK_THROWS(board.move(1,{3,0},Board::MoveDIR::Right));//out
+        CHECK_THROWS(board.move(2,{3,2},Board::MoveDIR::Up));//cant move
+        CHECK_THROWS(board.move(2,{2,2},Board::MoveDIR::Down));//cant move
+        CHECK_THROWS(board.move(2,{2,2},Board::MoveDIR::Right));//cant move
+        CHECK_THROWS(board.move(1,{4,1},Board::MoveDIR::Left));//wrong player
+        CHECK_THROWS(board.move(1,{3,2},Board::MoveDIR::Left));//wrong player
+        CHECK_THROWS(board.move(2,{0,3},Board::MoveDIR::Left));//wrong player
+
+
 }
