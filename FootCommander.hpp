bb954ec2525f870e4a5b31dfa52767907fe8c734
amitRan109 <57709369+amitRan109@users.noreply.github.com>
2020-06-03 21:03:28 +0300

    Update FootCommander.hpp

--- FootCommander.hpp
+++ FootCommander.hpp
@@ -1,11 +1,10 @@
 #pragma once
 #include "FootSoldier.hpp"
 #include <vector>
-namespace WarGame {
 class FootCommander : public FootSoldier {
 public:
     FootCommander (int p): Soldier(p,150,20,15) {}
     void act(std::vector <std::vector<Soldier*>> board,std::pair<int,int> my_loc);
     void act_all(std::vector <std::vector<Soldier*>> board,std::pair<int,int> my_loc);
 };
-}
+
