583a72b1892b7fa0fc39aac100006ef5a828383d
amitRan109 <57709369+amitRan109@users.noreply.github.com>
2020-06-03 21:03:45 +0300

    Update SniperCommander.hpp

--- SniperCommander.hpp
+++ SniperCommander.hpp
@@ -2,11 +2,9 @@
 #include "Sniper.hpp"
 #include <vector>
 
-namespace WarGame{
 class SniperCommander : public Sniper {
 public:
     SniperCommander (int p): Soldier(p,120,100,25) {}
     void act(std::vector <std::vector<Soldier*>> board,std::pair<int,int> my_loc);
     void act_all (std::vector <std::vector<Soldier*>> board,std::pair<int,int> my_loc);
 };
-}
