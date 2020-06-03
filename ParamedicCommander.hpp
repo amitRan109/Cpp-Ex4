0cc47681faac1b9e611838019ac505e874d74567
amitRan109 <57709369+amitRan109@users.noreply.github.com>
2020-06-03 21:03:09 +0300

    Update ParamedicCommander.hpp

--- ParamedicCommander.hpp
+++ ParamedicCommander.hpp
@@ -2,11 +2,9 @@
 #include "Paramedic.hpp"
 #include <vector>
 
-namespace WarGame{
 class ParamedicCommander : public Paramedic {
 public:
     ParamedicCommander (int p): Soldier(p,200,100,35) {}
     void act(std::vector <std::vector<Soldier*>> board,std::pair<int,int> my_loc);
     void act_all(std::vector <std::vector<Soldier*>> board,std::pair<int,int> my_loc);
 };
-}
